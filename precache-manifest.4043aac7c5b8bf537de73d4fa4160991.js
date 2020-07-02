b4f117aaf1ac984823492a94ee383c7b7d371d5e
seshu <nvseshaiah2013@gmail.com>
2020-07-02 21:52:52 +0530

    Updates

---
+++ precache-manifest.4043aac7c5b8bf537de73d4fa4160991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5450c2d9bd41c6eec4f48694cf48ac0",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "0a0105ec927dcc34bf39",
+    "url": "/covid-dashboard/static/css/main.a05ef3a3.chunk.css"
+  },
+  {
+    "revision": "036fa3d9d739906b1063",
+    "url": "/covid-dashboard/static/js/2.2cb6d6f7.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-dashboard/static/js/2.2cb6d6f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0105ec927dcc34bf39",
+    "url": "/covid-dashboard/static/js/main.b66b4737.chunk.js"
+  },
+  {
+    "revision": "1746489ac713bd07b55a",
+    "url": "/covid-dashboard/static/js/runtime-main.60ead92d.js"
+  }
+]);
