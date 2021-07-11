26343eef45926766a7e07dfc73b0b9f117c0b942
seshu <nvseshaiah2013@gmail.com>
2021-07-11 17:50:23 +0530

    Testing PWA

---
+++ public/service-worker.js
@@ -0,0 +1,48 @@
+/* eslint-disable no-restricted-globals */
+var CACHE_NAME = 'covid-dashboard-app';
+var urlsToCache = [
+  '/covid-dashboard'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  var cacheWhitelist = ['covid-dashboard-app'];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        // eslint-disable-next-line array-callback-return
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
